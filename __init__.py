9454b747cdf908b52fd7f0f3d8cccbdb6db41fec
volodiagamivka <gamivkavol@gmail.com>
2025-10-01 17:44:55 +0300

    added cloud db

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from my_project.db_init import db
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Gamivka1505@localhost/hospitals'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://volgam:Gamivka1505@hospital.mysql.database.azure.com:3306/hospital'
 
     db.init_app(app)
 
